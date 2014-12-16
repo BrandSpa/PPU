b0a7a246a71e9a2bb44f5c1af56b33ad63537655
Alejandro Sanabria <alesanabriav@gmail.com>
2014-12-16 17:10:11 -0500

    working on layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   layout :layout_by_resource
- def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+  def set_locale
+    if  request.subdomain == 'en'
+      I18n.locale = request.subdomain
+    end
+
+    I18n.locale = I18n.default_locale   
   end
+
   protect_from_forgery with: :exception
 
   def layout_by_resource
