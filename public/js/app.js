28b35002ed54877ed330badb9c5c4914949f0ae5
Alejandro Sanabria <alesanabriav@gmail.com>
2014-12-16 20:17:17 -0500

    added more desing

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+$(function() {
+  return ppu.AppView = (function(_super) {
+    __extends(AppView, _super);
+
+    function AppView() {
+      return AppView.__super__.constructor.apply(this, arguments);
+    }
+
+    AppView.prototype.el = $("#ppu-app");
+
+    AppView.prototype.events = {
+      "click .change-lang-page": 'changeLangPage'
+    };
+
+    AppView.prototype.changeLangPage = function(e) {
+      e.preventDefault();
+      if (lang === 'en') {
+        return window.location = "" + app.hostname + app.pathname;
+      } else {
+        return window.location = "en." + app.hostname + app.pathname;
+      }
+    };
+
+    return AppView;
+
+  })(Backbone.View);
+});
