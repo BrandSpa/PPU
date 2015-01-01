519680c8859a218a7b34d150a56bcb601c277c0b
Alejandro Sanabria <ale@Alejandros-MacBook-Pro.local>
2015-01-01 00:47:08 -0500

    added posts

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,11 +1,16 @@
 class Api::CategoriesController < ApplicationController
 	def show
-		model = Category.includes(:gallery, :translation, :experiences).find_by(slug: params[:id])
-		render json: model.to_json(:include => [:gallery, :translation, :translations, :experiences => {:include => :gallery} ])
+		if I18n.locale == "en"
+			lawyer_position = "Partner"
+		else
+			lawyer_position = "Socio"
+		end
+		model = Category.includes(:gallery, :translation, :experiences, :lawyers).joins(:lawyers).where("lawyers.position = ?", lawyer_position).find_by(slug: params[:id])
+		render json: model.to_json(:include => [:gallery, :translation, :translations, :lawyers, :experiences => {:include => :gallery} ])
 	end
 
   def index
-    lang = params[:lang] || I18n.locale 
+    lang = params[:lang] || I18n.locale
     collection = Category.includes(:gallery).all.lang(lang).order('name ASC')
     render json: collection.to_json(:include => [:gallery])
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,5 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :experiences
   belongs_to :gallery
   scope :lang, -> (lang){ where(lang: lang) }
+ 
 end
