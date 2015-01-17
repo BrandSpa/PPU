e18b6094cfa9a46d8f84df1b85d13a796682c521
Alejandro Sanabria <alesanabriav@gmail.com>
2015-01-16 20:02:25 -0500

    concat css [deploy:stage]

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -7,7 +7,6 @@ var gulp = require('gulp');
   concat = require('gulp-concat');
   uglify = require('gulp-uglify');
 
-
 gulp.task('coffee', function() {
   gulp.src(['coffee/*.coffee', 'coffee/*/*.coffee'])
     .pipe(coffee({bare: true}).on('error', gutil.log))
@@ -16,26 +15,20 @@ gulp.task('coffee', function() {
 
 gulp.task('sass', function () {
     gulp.src('sass/*.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('css'));
+      .pipe(sass())
+      .pipe(gulp.dest('css'))
 });
 
 gulp.task('stylesheets', function() {
   gulp.src([
-    'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/jquery-ui/jquery-ui.js',
-    'bower_components/bootstrap/dist/js/bootstrap.min.js',
-    'bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js',
-    'bower_components/underscore/underscore-min.js',
-    'bower_components/handlebars/handlebars.min.js',
-    'bower_components/backbone/backbone.js',
-    'bower_components/jquery.serializeJSON/jquery.serializejson.min.js',
-    'bower_components/moment/min/moment.min.js',
-    'bower_components/nprogress/nprogress.js',
-    'js/libs/jquery.selectBoxIt.min.js'
+    'css/theme.css',
+    'css/libs/selectBoxIt.css',
+    'bower_components/nprogress/nprogress.css'
     ])
-    .pipe(concat('dependencies.js'))
-    .pipe(gulp.dest('js/dist/'))
+    .pipe(concat('style.css'))
+    .pipe(minifyCSS())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('css/dist/'))
 });
 
 gulp.task('dependencies-scripts', function() {
@@ -84,9 +77,8 @@ gulp.task('compress', function() {
     .pipe(gulp.dest('js/dist/min'))
 });
 
-
 gulp.task('watch', ['sass', 'coffee'], function(){
-  gulp.watch('sass/*.sass', ['sass']);
+  gulp.watch('sass/*.sass', ['sass', 'stylesheets']);
   gulp.watch(['coffee/*.coffee', 'coffee/*/*.coffee'], ['coffee', 'app-scripts']);
 });
  
