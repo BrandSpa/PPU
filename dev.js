119b2659bd1be9e892b4d6b29719a96f667e8f8d
Alejandro Sanabria <alesanabriav@gmail.com>
2016-02-25 17:44:27 -0500

    last update from local

--- dev.js
+++ dev.js
@@ -1,2 +1,29 @@
-#! /usr/bin/env node
-console.log("console.log output")
+'use strinct';
+var serverExec = require('ssh-exec');
+var localExec = require('child_process').exec;
+var moment = require('moment');
+var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+var serverFlow = [
+  "cd apps/ppu-pruebas",
+  "touch random.js",
+  "git add -A",
+  "git commit -m 'last update from server on "+ now +"'",
+  "git pull origin master"
+].join(' && ');
+
+
+function serverMessages(err, stdout, stderr) {
+  if(err) console.log("error: ", err);
+  if(stderr) console.log("stderr: ", stderr);
+  console.log("stdout: ", stdout);
+}
+
+localExec('git add -A && git commit -m "last update from local"', function (err, stdout, stderr) {
+  if (err) console.error(err);
+  console.log(stdout);
+});
+
+serverExec(serverFlow, 'deploy@104.131.161.42', serverMessages);
+
+
