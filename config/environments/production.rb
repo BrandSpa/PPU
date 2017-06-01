f2d94cd484eb79e2080a2d5cfc6c8f4da52fde1a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-01 13:07:00 -0500

    change pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
       port:                 587,
       domain:               'ppulegal.com',
       user_name:            'alejandro@brandspa.com',
-      password:             'g73AdHj3QBS88zxLdbiXLw',
+      password:             'Durd3n_99',
       authentication:       'plain',
       enable_starttls_auto: true  }
 end
