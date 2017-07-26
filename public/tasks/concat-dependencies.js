5b3d97db103dc3550b999f1cb5b9db33bea11b43
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-26 15:06:18 -0500

    add datapicker

--- public/tasks/concat-dependencies.js
+++ public/tasks/concat-dependencies.js
@@ -5,13 +5,14 @@ var concat = require('gulp-concat');
 
 gulp.task('concat_dependencies', function() {
   gulp.src([
+    'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css',
     'node_modules/bootstrap/dist/css/bootstrap.css',
     'css/default.css',
     'css/dist/theme.css',
     'node_modules/font-awesome/css/font-awesome.css',
     'node_modules/loaders.css/loaders.css',
     'node_modules/ionicons/css/ionicons.css',
-    '/node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css'
+
   ])
   .pipe(concat('dependencies.css'))
   .pipe(gulp.dest('css/dist'));
